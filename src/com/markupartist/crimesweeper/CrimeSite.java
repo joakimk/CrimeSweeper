015d413f6a8a93b2dc971d65529d0233165c0448
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2009-11-28 17:27:39 +0100

    Cleaned up the code in CrimeSite and added comments about the strange parts.

--- src/com/markupartist/crimesweeper/CrimeSite.java
+++ src/com/markupartist/crimesweeper/CrimeSite.java
@@ -30,9 +30,7 @@ public class CrimeSite extends GeoPoint {
 	float collisionDistance = 20.0f;
 	private static final String TAG = "Collision: ";
     private String _title;
-	int _lat;
-	int _lng;    
-	
+    	
 	String getTitle()
 	{
 		return _title;
@@ -52,19 +50,16 @@ public class CrimeSite extends GeoPoint {
 	{
 		List<CrimeSite> crimeSites = new ArrayList<CrimeSite>();
 		
-		DocumentBuilder docBuilder = null;
 		try {
-			docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-			URL url = new URL("http://brottsplatsstockholm.se/api.php?action=getEvents&period=" + minutes);	
-			Document xmlDoc = docBuilder.parse(url.openConnection().getInputStream());
-			NodeList nodes = xmlDoc.getChildNodes().item(0).getChildNodes();
-			//NodeList name = nodes.item(0).getChildNodes();
-			for(int i = 1; i < nodes.getLength(); i+=2)
-			{
-				int lat = (int)(Float.valueOf(getValue(nodes.item(i), "lat")).doubleValue() * 1000000);
-				int lng = (int)(Float.valueOf(getValue(nodes.item(i), "lng")).doubleValue() * 1000000);									
-				crimeSites.add(new CrimeSite(getValue(nodes.item(i), "titleCleaned"), lat, lng));
-			}
+			Document xmlDoc = loadDocument("http://brottsplatsstockholm.se/api.php?action=getEvents&period=" + minutes);
+			NodeList nodes = getEventNodes(xmlDoc);
+			
+			// NOTE: We skip every other element because it's a "text" element.
+			// TODO: Look into why this is so, and if we're misusing this API somehow :)
+			for(int i = 1; i < nodes.getLength(); i+=2)									
+				crimeSites.add(new CrimeSite(getValueByElementName(nodes.item(i), "titleCleaned"),
+											 getCordinate(nodes.item(i), "lat"),
+											 getCordinate(nodes.item(i), "lng")));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -73,43 +68,54 @@ public class CrimeSite extends GeoPoint {
 		return crimeSites;
 	}
 
-//    static List<CrimeSite> getCrimeSites(int minutes) {
-//        List<CrimeSite> crimeSites = new ArrayList<CrimeSite>();
-//        crimeSites.add(new CrimeSite("Grand Theft Auto", 59414207, 18273497));
-//        crimeSites.add(new CrimeSite("Murder One", 59514207, 18173497));
-//        crimeSites.add(new CrimeSite("Close to me", 59279986, 1808275));
-//
-//        return crimeSites;
-//    }
-//	
 	CrimeSite(String title, int lat, int lng) {
         super(lat, lng);
 		_title = title;
+	}
+	
+private
+
+	static NodeList getEventNodes(Document xmlDoc)
+	{
+		return xmlDoc.getChildNodes().item(0).getChildNodes();
+	}
+
+	static Document loadDocument(String url) throws MalformedURLException, SAXException, IOException, ParserConfigurationException, FactoryConfigurationError
+	{
+		DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		return docBuilder.parse(new URL(url).openConnection().getInputStream());	
+	}
+
+	static int getCordinate(Node node, String name)
+	{
+		return (int)(Float.valueOf(getValueByElementName(node, name)).doubleValue() * 1000000);
+	}
+
+	static String getValueByElementName(Node eventNode, String elementName)
+	{
+		NodeList eventChildren = eventNode.getChildNodes();
+		for(int i = 1; i < eventChildren.getLength(); i += 2)
+		{
+			Node node = eventChildren.item(i);
+			if(node.getNodeName().equals(elementName))
+				return getValueForNode(node);
+		}
+		
+		return "";
+	}
+	
+	static String getValueForNode(Node node)
+	{
+		// TODO: Why do we have to do this? Seems a bit odd.
+		NodeList contentNodes = node.getChildNodes();
+		Node cdata_node = contentNodes.item(contentNodes.getLength() - 1);
+		return cdata_node.getNodeValue();		
 	}
 
 	/*
 	 * The great circle distance d between two points with coordinates {lat1,lon1} and {lat2,lon2} is given by:
 	 * d=acos(sin(lat1)*sin(lat2)+cos(lat1)*cos(lat2)*cos(lon1-lon2))
 	 */
-	
-private
-
-	static String getValue(Node eventNode, String elementName) throws Exception
-	{
-		NodeList eventChildren = eventNode.getChildNodes();
-		for(int i = 1; i < eventChildren.getLength(); i += 2)
-		{
-			Node node = eventChildren.item(i);
-			if(node.getNodeName().equals(elementName)) {		
-				NodeList contentNodes = node.getChildNodes();
-				Node cdata_node = contentNodes.item(contentNodes.getLength() - 1);
-				return cdata_node.getNodeValue();
-			}
-		}
-		
-		return "";
-	}
-	
 	double distanceBetweenCoords(int lat1, int lon1, int lat2, int lon2) {
 		return Math.acos( 
 			Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2)
