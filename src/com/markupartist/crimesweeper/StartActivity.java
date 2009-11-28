772ec91aa79d7d66d51736f8c22cdaf3ee9fe340
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2009-11-28 17:41:24 +0100

    Removed unused imports.

--- src/com/markupartist/crimesweeper/StartActivity.java
+++ src/com/markupartist/crimesweeper/StartActivity.java
@@ -11,7 +11,6 @@ import android.widget.Button;
 import android.view.View;
 import android.app.Dialog;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import com.google.android.maps.*;
 
 import java.util.List;
